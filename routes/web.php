6285a28e01d73bf424f3f1f0858c390eda728306
danielv22 <daniel.valencia22772@ucaldas.edu.co>
2023-11-17 10:45:20 -0500

    Middelware y algunos productos desde el controlador.
    Se implementó validación de rol y se mostraron algunos registros de productos en la vista principal

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'getAvailableProducts']);
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
+    'client_rol'
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
